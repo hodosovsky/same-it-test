131efab7b0eb35208a6088f91ffcfed3455f51ab
hodosovsky <107750587+hodosovsky@users.noreply.github.com>
2023-08-06 19:17:17 +0300

    add theme

---
+++ src/theme/theme.js
@@ -0,0 +1,41 @@
+const theme = {
+  colors: {
+    bg: '#F0F3F4',
+    textP: '#A93226',
+    title: '#D7DBDD',
+    border: '#979A9A',
+    hover: '#1EE83A',
+    hoverText: '#F7DC6F',
+    btnBg: '#FEF9E7',
+    btnHover: '#32EFDB',
+    backdrop: 'rgba(0, 0, 0, 0.2)',
+    modalBg: '#FBFCFC',
+  },
+
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  sizes: {
+    w: { full: '100vw', max: '100%', med: '50%', min: '25%' },
+    h: { full: '100vh', max: '100%', min: '30px' },
+  },
+  fontWeights: {
+    nornal: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    heading: 700,
+  },
+
+  borders: {
+    none: 'none',
+    primary: '1px solid',
+    secondary: '3px solid',
+  },
+  radii: {
+    min: '4px',
+    med: '6px',
+    max: '8px',
+  },
+};
+
+export default theme;
